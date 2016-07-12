1273c2269c36c4326c28ed5a3a134d172d9c2a3f
Santiago Cardenas <sancard@amazon.com>
2016-07-11 17:25:57 -0700

    Updated script to use AWS Quick Start PS Module

--- scripts/Configure-RDGW.ps1
+++ scripts/Configure-RDGW.ps1
@@ -1,50 +1,65 @@
-﻿param(
-    $ServerFQDN,
-    $DomainNetBiosName,
-    $GroupName,
-    $UserName
-    )
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$ServerFQDN,
 
-Import-Module remotedesktopservices
+    [Parameter(Mandatory=$true)]
+    [string]$DomainNetBiosName,
 
-$name = new-object -com "X509Enrollment.CX500DistinguishedName.1"
-$name.Encode("CN=$ServerFQDN", 0)
+    [Parameter(Mandatory=$true)]
+    [string]$GroupName
+)
 
-$key = new-object -com "X509Enrollment.CX509PrivateKey.1"
-$key.ProviderName = "Microsoft RSA SChannel Cryptographic Provider"
-$key.KeySpec = 1
-$key.Length = 1024
-$key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)"
-$key.MachineContext = 1
-$key.Create()
+try {
+    $ErrorActionPreference = "Stop"
 
-$serverauthoid = new-object -com "X509Enrollment.CObjectId.1"
-$serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
-$ekuoids = new-object -com "X509Enrollment.CObjectIds.1"
-$ekuoids.add($serverauthoid)
-$ekuext = new-object -com "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
-$ekuext.InitializeEncode($ekuoids)
+    Start-Transcript -Path c:\cfn\log\Configure-RDGW.ps1.txt -Append
 
-$cert = new-object -com "X509Enrollment.CX509CertificateRequestCertificate.1"
-$cert.InitializeFromPrivateKey(2, $key, "")
-$cert.Subject = $name
-$cert.Issuer = $cert.Subject
-$cert.NotBefore = get-date
-$cert.NotAfter = $cert.NotBefore.AddDays(730)
-$cert.X509Extensions.Add($ekuext)
-$cert.Encode()
+    Import-Module remotedesktopservices
 
-$enrollment = new-object -com "X509Enrollment.CX509Enrollment.1"
-$enrollment.InitializeFromRequest($cert)
-$certdata = $enrollment.CreateRequest(0)
-$enrollment.InstallResponse(2, $certdata, 0, "")
+    $name = new-object -com "X509Enrollment.CX500DistinguishedName.1"
+    $name.Encode("CN=$ServerFQDN", 0)
 
-dir cert:\localmachine\my | ? { $_.Subject -eq "CN=$ServerFQDN” } | % { [system.IO.file]::WriteAllBytes("c:\$env:computername.cer", ($_.Export('CERT', 'secret')) ) }
+    $key = new-object -com "X509Enrollment.CX509PrivateKey.1"
+    $key.ProviderName = "Microsoft RSA SChannel Cryptographic Provider"
+    $key.KeySpec = 1
+    $key.Length = 1024
+    $key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)"
+    $key.MachineContext = 1
+    $key.Create()
 
-new-item -path RDS:\GatewayServer\CAP -Name Default-CAP -UserGroups "$GroupName@$DomainNetBiosName" -AuthMethod 1
+    $serverauthoid = new-object -com "X509Enrollment.CObjectId.1"
+    $serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
+    $ekuoids = new-object -com "X509Enrollment.CObjectIds.1"
+    $ekuoids.add($serverauthoid)
+    $ekuext = new-object -com "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
+    $ekuext.InitializeEncode($ekuoids)
 
-new-item -Path RDS:\GatewayServer\RAP -Name Default-RAP -UserGroups "$GroupName@$DomainNetBiosName" -ComputerGroupType 2
+    $cert = new-object -com "X509Enrollment.CX509CertificateRequestCertificate.1"
+    $cert.InitializeFromPrivateKey(2, $key, "")
+    $cert.Subject = $name
+    $cert.Issuer = $cert.Subject
+    $cert.NotBefore = get-date
+    $cert.NotAfter = $cert.NotBefore.AddDays(730)
+    $cert.X509Extensions.Add($ekuext)
+    $cert.Encode()
 
-dir cert:\localmachine\my | where-object { $_.Subject -eq "CN=$ServerFQDN” } | ForEach-Object { Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $_.Thumbprint }
+    $enrollment = new-object -com "X509Enrollment.CX509Enrollment.1"
+    $enrollment.InitializeFromRequest($cert)
+    $certdata = $enrollment.CreateRequest(0)
+    $enrollment.InstallResponse(2, $certdata, 0, "")
 
-Restart-Service tsgateway
+    dir cert:\localmachine\my | ? { $_.Subject -eq "CN=$ServerFQDN” } | % { [system.IO.file]::WriteAllBytes("c:\$env:computername.cer", ($_.Export('CERT', 'secret')) ) }
+
+    new-item -path RDS:\GatewayServer\CAP -Name Default-CAP -UserGroups "$GroupName@$DomainNetBiosName" -AuthMethod 1
+
+    new-item -Path RDS:\GatewayServer\RAP -Name Default-RAP -UserGroups "$GroupName@$DomainNetBiosName" -ComputerGroupType 2
+
+    dir cert:\localmachine\my | where-object { $_.Subject -eq "CN=$ServerFQDN” } | ForEach-Object { Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $_.Thumbprint }
+
+    Restart-Service tsgateway
+}
+catch {
+    Write-Verbose "$($_.exception.message)@ $(Get-Date)"
+    $_ | Write-AWSQuickStartException
+}
