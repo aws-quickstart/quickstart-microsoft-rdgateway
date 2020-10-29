ce1ace3abc7621c0c4fc85abeb8ec06be20607f7
virtlima <aarolima@amazon.com>
2020-10-29 17:48:24 -0400

    Moving to SSM Completely (#30)
    * updated rdgw-domain to use SSM, updated Instance types
    
    * added listeners and tweaked IAM Roles to be less permissive
    
    * updated standalone template to use SSM
    
    * updated instance options
    
    * modified step name

--- scripts/Create-LocalUser.ps1
+++ scripts/Create-LocalUser.ps1
@@ -1,24 +1,36 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$AdminUser,
+    [Parameter(Mandatory = $true)][string]$AdminSecParam,
 
-    [Parameter(Mandatory=$true)]
-    [string]$AdminPassword,
+    [Parameter(Mandatory=$false)][string]$FullName,
 
-    [Parameter(Mandatory=$false)]
-    [string]$FullName,
-
-    [Parameter(Mandatory=$false)]
-    [string]$Description
+    [Parameter(Mandatory=$false)][string]$Description
 )
+
+# Getting Password from Secrets Manager for AD Admin User
+Try {
+    $AdminSecret = Get-SECSecretValue -SecretId $AdminSecParam -ErrorAction Stop | Select-Object -ExpandProperty 'SecretString'
+} Catch [System.Exception] {
+    Write-Output "Failed to get $AdminSecParam Secret $_"
+    Exit 1
+}
+
+Try {
+    $AdminPassword = ConvertFrom-Json -InputObject $AdminSecret -ErrorAction Stop
+} Catch [System.Exception] {
+    Write-Output "Failed to convert AdminSecret from JSON $_"
+    Exit 1
+}
+
+$AdminUser = $AdminPassword.UserName
 
 try {
     $ErrorActionPreference = "Stop"
     Start-Transcript -Path C:\cfn\log\$($MyInvocation.MyCommand.Name).log -Append
 
-    $SecureString = ConvertTo-SecureString -AsPlainText $AdminPassword -Force
+    $SecureString = ConvertTo-SecureString ($AdminPassword.Password) -AsPlainText -Force
     New-LocalUser $AdminUser -Password $SecureString -FullName $FullName -Description $Description
+    net localgroup Administrators $AdminUser /ADD
 }
 catch {
     $_ | Write-AWSQuickStartException
